5ec460fd111f543161ec3d54a021c5b40213b546
BlueSkeye <M2438FE89@gmail.com>
2021-02-19 18:01:21 +0100

    Initial commit

---
+++ HttpClient.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace ApiAuth
+{
+    public class HttpClient
+    {
+        private const string AuthorizationContentHeaderName =
+            "X-Authorization-Content-SHA256";
+        private const int NoGcRegionSize = 16 * 1024 * 1024;
+        private static readonly object _noGcLock = new object();
+        private SecureString _apiKeyId;
+        private SecureString _apiSecretKey;
+
+        private HttpClient()
+        {
+        }
+
+        public string ApiKeyId
+        {
+            set
+            {
+                _apiKeyId = new SecureString();
+                foreach(char scannedCharacter in value
+                    ?? throw new ArgumentNullException("ApiKeyId")) {
+                    _apiKeyId.AppendChar(scannedCharacter);
+                }
+                _apiKeyId.MakeReadOnly();
+            }
+        }
+
+        public string ApiSecretKey
+        {
+            set
+            {
+                _apiSecretKey = new SecureString();
+                foreach (char scannedCharacter in value
+                    ?? throw new ArgumentNullException("ApiSecretKey"))
+                {
+                    _apiSecretKey.AppendChar(scannedCharacter);
+                }
+                _apiSecretKey.MakeReadOnly();
+            }
+        }
+
+        public static HttpClient Create()
+        {
+            return new HttpClient();
+        }
+
+        private static string CreateCanonicalString(HttpWebRequest request)
+        {
+            WebHeaderCollection headers =
+                (request ?? throw new ArgumentNullException(nameof(request))).Headers;
+            StringBuilder canonicalBuilder = new StringBuilder();
+            canonicalBuilder.Append(request.Method
+                ?? throw new InvalidOperationException("Method not defined"));
+            canonicalBuilder.Append(',');
+            canonicalBuilder.Append(request.ContentType ?? string.Empty);
+            canonicalBuilder.Append(',');
+            canonicalBuilder.Append(headers.Get(AuthorizationContentHeaderName));
+            canonicalBuilder.Append(',');
+            canonicalBuilder.Append((request.RequestUri
+                ?? throw new InvalidOperationException("Request path undefined"))
+                    .AbsolutePath
+                        ?? throw new InvalidOperationException("Path undefined"));
+            canonicalBuilder.Append(',');
+            canonicalBuilder.Append(DateTime.UtcNow.ToString("R"));
+            return canonicalBuilder.ToString();
+        }
+
+        public HttpWebResponse Send(HttpWebRequest request)
+        {
+            string canonicalString = CreateCanonicalString(request);
+            string base64Signature = GetAuthorizationString(canonicalString);
+            request.Headers.Add("Authorization", base64Signature);
+            return (HttpWebResponse)request.GetResponse();
+        }
+
+        private string GetAuthorizationString(string canonicalString)
+        {
+            byte[] rawCanonicalString = UnicodeEncoding.Unicode.GetBytes(canonicalString
+                ?? throw new ArgumentNullException(nameof(canonicalString)));
+            byte[] signature;
+            using (HMACSHA256 signatureAlgorithm = (HMACSHA256)HMACSHA256.Create()) {
+                IntPtr rawSecretKey = IntPtr.Zero;
+                byte[] transientSecretKey = null;
+                lock (_noGcLock) {
+                    // Remark : Secret key may be leaked in memory if a GC occurs between
+                    // marshaling conversion and finally block completion.
+                    GC.TryStartNoGCRegion(1024);
+                    try {
+                        try {
+                            rawSecretKey = Marshal.SecureStringToCoTaskMemUnicode(_apiSecretKey);
+                            transientSecretKey = Marshal.PtrToStructure<byte[]>(rawSecretKey);
+                        }
+                        finally {
+                            if (IntPtr.Zero != rawSecretKey) {
+                                Marshal.ZeroFreeCoTaskMemUnicode(rawSecretKey);
+                            }
+                        }
+                        try {
+                            // TODO : Check accessor source code. Should the key be copied
+                            // in an objet local array, some additional security measure
+                            // should be enforced.
+                            signatureAlgorithm.Key = transientSecretKey;
+                            signature = signatureAlgorithm.ComputeHash(rawCanonicalString);
+                        }
+                        finally {
+                            int transientSecretKeylength = transientSecretKey.Length;
+                            for(int index = 0; index < transientSecretKeylength; index++) {
+                                transientSecretKey[index] = 0;
+                            }
+                        }
+                    }
+                    finally {
+                        GC.EndNoGCRegion();
+                    }
+                }
+            }
+            return Convert.ToBase64String(signature);
+        }
+    }
+}
